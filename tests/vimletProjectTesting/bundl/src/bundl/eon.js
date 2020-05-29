26d833f0ffe3cb3cf1c9822d962abe47630ca6cd
jesusVimlet <jperefel@gmail.com>
2020-05-29 07:35:47 +0100

    Removed tobeio tests from bundl test

--- tests/vimletProjectTesting/bundl/src/bundl/eon.js
+++
@@ -1,9 +0,0 @@
-module.exports = async function (entry, bundl) {
-    await bundl.run.exec("npx", {
-        args: ["eon-cli", "build"],
-        execHandler: function (out, error) {
-            console.log(out || error);
-        }
-    });
-    return entry;
-};
