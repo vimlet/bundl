95b62eb3c8b340b9a9a4bf45c077c6e599377355
jesusvimlet <jperefel@gmail.com>
2025-07-09 15:45:50 +0100

    Fixes upgrade node

--- src/lib/parse.js
+++ src/lib/parse.js
@@ -1,25 +1,15 @@
-const {
-  promisify
-} = require("util");
-const meta = require("@vimlet/meta");
-const parse = promisify(meta.parse);
+const meta = require("@vimlet/meta").instance();
 
 meta.sandbox = {
-  "hash": function (id) {          
-    if (this.data.__meta && this.data.__meta[id] && this.data.__meta[id].hash) {
-      return this.data.__meta[id].hash;
-    }
+  hash(id) {
+    return this.data.__meta?.[id]?.hash;
   },
-  "file": function (id) {
-    if (this.data.__meta && this.data.__meta[id] && this.data.__meta[id].file) {
-      this.echo(this.data.__meta[id].file);
-    }
+  file(id) {
+    this.echo(this.data.__meta?.[id]?.file || "");
   },
-  "filename": function (id) {
-    if (this.data.__meta && this.data.__meta[id] && this.data.__meta[id].filename) {
-      this.echo(this.data.__meta[id].filename);
-    }
+  filename(id) {
+    this.echo(this.data.__meta?.[id]?.filename || "");
   }
 };
 
-module.exports = parse;
+module.exports = meta.parse;
